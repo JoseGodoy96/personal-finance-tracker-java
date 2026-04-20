7675316b1ed6b951a408cdd33f32dc57977b4948
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-20 23:02:05 +0100

    <feat>(Transaction): Creada la logica de Transaction

--- smart-expense-tracker/src/main/java/com/chema/db/smartexpensetracker/model/Transaction.java
+++ smart-expense-tracker/src/main/java/com/chema/db/smartexpensetracker/model/Transaction.java
@@ -1,4 +1,60 @@
 package com.chema.db.smartexpensetracker.model;
 
+import java.time.LocalDate;
+
 public class Transaction {
+
+    private static int counter = 1;
+
+    private int id;
+    private double amount;
+    private Category category;
+    private LocalDate date;
+    private String description;
+    private TransactionType type;
+
+    public Transaction(double amount, Category category, LocalDate date, String description, TransactionType type) {
+        this.id = counter++;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.description = description;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    @Override
+    public  String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", category=" + category +
+                ", date=" + date +
+                ", description='" + description + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
