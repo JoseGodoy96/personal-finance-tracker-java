e81d411a887723214ec555a911584cffb22d30bb
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-25 17:20:33 +0100

    <feat>(ConsoleMenu.java): Creada la logica de los expenses igual pero al contrario creada una arraylist de transaction para que viva el balanca de momento antes de conectarlo con una base de datos

--- smart-expense-tracker/src/main/java/com/chema/db/smartexpensetracker/ui/ConsoleMenu.java
+++ smart-expense-tracker/src/main/java/com/chema/db/smartexpensetracker/ui/ConsoleMenu.java
@@ -1,15 +1,17 @@
 package com.chema.db.smartexpensetracker.ui;
 
+import com.chema.db.smartexpensetracker.model.Transaction;
 import com.chema.db.smartexpensetracker.model.TransactionType;
 import com.chema.db.smartexpensetracker.service.TransactionService;
-
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Scanner;
 import com.chema.db.smartexpensetracker.service.TransactionService;
 import com.chema.db.smartexpensetracker.model.Category;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.format.DateTimeFormatter;
+
 
 public class ConsoleMenu {
 
@@ -34,6 +36,8 @@ public class ConsoleMenu {
 
             int option = sc.nextInt();
 
+            ArrayList<Transaction> transactions = new ArrayList<>();
+
             switch (option) {
                 case 1:
                     double value;
@@ -43,6 +47,7 @@ public class ConsoleMenu {
                         if (!sc.hasNextDouble()) {
                             System.out.println("Invalid input. Please enter a number.");
                             sc.next();
+
                             continue;
                         }
                         value = sc.nextDouble();
@@ -103,13 +108,93 @@ public class ConsoleMenu {
                             continue;
                         }
 
-                        TransactionType type = TransactionType.INCOME;
-                        
+
                         break;
                     }
 
+                    TransactionType type = TransactionType.INCOME;
+
+                    Transaction transaction = new Transaction(value, category, date, description, type);
+                    transactions.add(transaction);
+                    System.out.println("Income added successfully.");
                     break ;
                 case 2:
+                    double valueExpense;
+
+                    while (true) {
+                        System.out.println("Please enter the expense value:");
+                        if (!sc.hasNextDouble()) {
+                            System.out.println("Invalid input. Please enter a number.");
+                            sc.next();
+                            continue;
+                        }
+                        valueExpense = sc.nextDouble();
+                        sc.nextLine();
+                        if (valueExpense < 0) {
+                            System.out.println("Value must be 0 or greater.");
+                            continue;
+                        }
+                        break;
+                    }
+
+                    Category categoryExpense = null;
+                    for (Category c : Category.values()) {
+                        System.out.println("- " + c);
+                    }
+
+                    while (categoryExpense == null) {
+                        System.out.println("Select a category from the list:");
+                        String input = sc.nextLine();
+
+                        try {
+                            categoryExpense = Category.valueOf(input.toUpperCase());
+                        } catch (IllegalArgumentException e) {
+                            System.out.println("Invalid category. try again");
+                        }
+                    }
+
+                    DateTimeFormatter formatterExpense = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+                    LocalDate dateExpense = null;
+                    while (dateExpense == null) {
+                        System.out.println("Please enter a date (YYYY-MM-DD): ");
+                        String dateInput = sc.nextLine();
+                        try {
+                            dateExpense = LocalDate.parse(dateInput, formatterExpense);
+                            if (dateExpense.isAfter(LocalDate.now())) {
+                                System.out.println("Date cannot be in the future");
+                                dateExpense = null;
+                            }
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Invalid format. please enter YYYY-MM-DD");
+                        }
+                    }
+
+                    String descriptionExpense;
+
+                    while (true) {
+                        System.out.print("Please enter a description: ");
+                        descriptionExpense = sc.nextLine().trim();
+
+                        if (descriptionExpense.isEmpty()) {
+                            System.out.println("Description can't be empty");
+                            continue;
+                        }
+
+                        if (descriptionExpense.length() < 3) {
+                            System.out.println("must be at least 3 characters");
+                            continue;
+                        }
+
+
+                        break;
+                    }
+
+                    TransactionType typeExpense = TransactionType.EXPENSE;
+
+                    Transaction transactionExpense = new Transaction(valueExpense, categoryExpense, dateExpense, descriptionExpense, typeExpense);
+                    transactions.add(transactionExpense);
+                    System.out.println("Expense added successfully.");
                     break ;
                 case 3:
                     break ;
